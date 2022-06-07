082d6c3db56115a63957cb77168e9c974a992d34
rick <rick@rickbergmann.com>
2022-06-07 23:35:12 +0100

    Add footer svg icons

--- src/components/layoutComponents/footer/footer.js
+++ src/components/layoutComponents/footer/footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import NpmIcon from '../../svgIcons/npmIcon';
+import GithubIcon from '../../svgIcons/githubIcon';
+import YarnIcon from '../../svgIcons/yarnIcon';
 
 const currentDate = new Date();
 
@@ -12,19 +15,26 @@ const Footer = () => {
             target="_blank"
             rel="noreferrer"
           >
-            Github Project
+            <GithubIcon />
           </a>
-          {' | '}
           <a
             href="https://www.npmjs.com/package/less-style-library"
             target="_blank"
             rel="noreferrer"
           >
-            NPM Package
+            <NpmIcon />
+          </a>
+          <a
+            href="https://yarnpkg.com/package/less-style-library"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <YarnIcon />
           </a>
         </p>
         <div className={`footer-content-disclaimer`}>
           <small>
+            Less Style Library is open source and released under the MIT licence <br />
             &copy;
             {` Copyright ${currentDate.getFullYear()} rickbergmann.com`}
           </small>
