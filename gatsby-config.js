b445715dd146772cd63958abd003639856eac5db
rick <rick@rickbergmann.com>
2022-06-13 00:03:34 +0100

    Fix layout grid on mobile

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,17 @@ module.exports = {
         // removeStyleLinkTypeAttributes: false,
       }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    }
   ],
 };
